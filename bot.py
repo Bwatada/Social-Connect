30acebcd95e70e3ff95c5f06db8c2b26db16dacf
Bwatada <brandonwatada@gmail.com>
2017-12-03 06:35:26 -0500

    added \!help function

--- bot.py
+++ bot.py
@@ -60,6 +60,7 @@ async def on_message(message):
 				page_access_token = page["access_token"]
 			graph = facebook.GraphAPI(page_access_token)
 		success = graph.put_wall_post(message)
-       
-
+    if message.content == "!help":
+        await bot.send_message(message.channel, "Usage: !setPost - sets the desired channel for announcements to be made !post [message]- posts entered message to predetermined facebook page. !read** - reads latest post from Facebook page and posts it in desired channel.")
+        await bot.send_message(message.channel, "In order to use this bot, you must have the facebook module and discord.py modules installed. You must also be able to run the program in a terminal. We are planning to eventually host the bot on a server so you will be able to add it that way instead. When using the bot for the first time, use the command !setPost in the desired channel in order to tell the bot that this is where you would like all announcements to be posted. "
 bot.run('MzU3NzIzNzYyMDkyODAyMDUw.DQP9Eg.K4m6VXRtCZMlvvO53uE51mraANs')
